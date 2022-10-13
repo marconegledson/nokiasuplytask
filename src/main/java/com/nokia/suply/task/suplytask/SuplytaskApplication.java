1bc1340b6e6464c9c7529f26e07d77a3073866e5
Marcone <Marcone@Users-MacBook-Pro.local>
2022-10-13 11:27:59 -0300

    initial commit

---
+++ src/main/java/com/nokia/suply/task/suplytask/SuplytaskApplication.java
@@ -0,0 +1,13 @@
+package com.nokia.suply.task.suplytask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuplytaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuplytaskApplication.class, args);
+    }
+
+}
